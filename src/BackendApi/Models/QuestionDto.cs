0008a113ae0e8977ec0cf6a79da0c4abf55c4505
Pavel Antropov <p.m.antropov@gmail.com>
2023-03-02 23:37:14 +0400

    Update QuestionDto

--- src/BackendApi/Models/QuestionDto.cs
+++ src/BackendApi/Models/QuestionDto.cs
@@ -2,5 +2,8 @@
 
 public class QuestionDto
 {
+	public int? Id { get; set; }
     public string Text { get; set; }
+	public List<string> CorrectAnswers { get; set; }
+	public List<string> WrongAnswers { get; set; }
 }
