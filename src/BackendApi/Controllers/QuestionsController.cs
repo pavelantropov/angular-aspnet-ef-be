d78176e8a8f23f144ea571fa5ff5bae44b935685
Pavel Antropov <p.m.antropov@gmail.com>
2023-03-02 23:37:36 +0400

    Add endpoints

--- src/BackendApi/Controllers/QuestionsController.cs
+++ src/BackendApi/Controllers/QuestionsController.cs
@@ -7,8 +7,30 @@ namespace Api.Controllers;
 [ApiController]
 public class QuestionsController : ControllerBase
 {
-	[HttpPost]
-	public async Task<ActionResult> PostQuestion([FromBody]QuestionDto question)
+	private readonly QuizContext _context;
+
+	public QuestionsController(QuizContext context)
+	{
+		_context = context;
+	}
+
+	[Route(""), HttpGet]
+	public async Task<ActionResult> GetQuestions()
+	{
+		return Ok(_context.Questions);
+	}
+
+	[Route(""), HttpPost]
+	public async Task<ActionResult> PostQuestion([FromBody]QuestionDto question, CancellationToken cancellation)
+	{
+		await _context.Questions.AddAsync(question, cancellation);
+		await _context.SaveChangesAsync(cancellation);
+
+		return Ok();
+	}
+
+	[Route("chatGpt"), HttpPost]
+	public async Task<ActionResult> AskChatGpt([FromBody] QuestionDto question)
 	{
 		return Ok();
 	}
