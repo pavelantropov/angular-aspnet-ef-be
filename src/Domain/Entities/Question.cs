5ca68f7a3695cb6faad07042c094b24c8f6df9b6
Pavel Antropov <p.m.antropov@gmail.com>
2023-03-03 22:00:34 +0400

    Create new projects

---
+++ src/Domain/Entities/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Question
+{
+	public int Id { get; set; }
+	public string Text { get; set; }
+	public List<string> CorrectAnswers { get; set; }
+	public List<string> WrongAnswers { get; set; }
+}
